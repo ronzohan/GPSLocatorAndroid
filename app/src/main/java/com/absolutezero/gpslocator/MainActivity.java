4840ffca57b906c5f099c161523c4fd1cefbe7be
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-12-06 01:01:41 +0800

    Added menu

--- app/src/main/java/com/absolutezero/gpslocator/MainActivity.java
+++ app/src/main/java/com/absolutezero/gpslocator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.absolutezero.gpslocator;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container, new MainFragment());
+        ft.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
